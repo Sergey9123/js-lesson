c4927a1849d86e5368b8c447d322ece012c73afe
Sergey9123 <ser.khach9@gamil.com>
2021-04-28 15:55:46 +0400

    Prototypes

--- jsGyumri/test.js
+++ jsGyumri/test.js
@@ -1,139 +1,239 @@
 
-// function sumTo(n){
-//     let sum = 0;
-//   if(n == 1){
-//       return 1;
-//  }else{
-//      sum =  n + sumTo(n-1);
+// let array = [2,3,4,1,10,9,7,5,6];
+// let array1 = [2,3,4,8,10,9,7,5,6,1];
+// let sum = 0;
+// let allSum = 0;
+// let missingNumber = 0;
+
+// function count(array1){
+//    for(let i = 0; i < array1.length;i+=1){
+//        allSum = allSum + array1[i]; 
+//    }
+//     return allSum ;  
+// }
+
+// count(array1);
+
+
+// for(let i = 0; i < array.length;i++){
+//     sum = sum + array[i];
+//     missingNumber = allSum - sum ;
+    
+// }
+
+// console.log(missingNumber);
+
+ // version of internet 
+
+
+// let arr = [2,3,1,4,8,6,5,9,10];
+// let i = 1;
+
+// while(arr.indexOf(i) !== -1) {
+//   i++;
+// }
+
+// console.log(i);
+
+// let array = [2,3,4,1,10,9,7,5,6];
+//  let sum = 0;
+//  let count = 10;
+
+
+// for(let i = 0; i < array.length;i++){
+//      count = i+1+count;
+//         sum += array[i];
+    
 //  }
-//   return sum;
-// }
-// console.log(sumTo(10));
 
-// function sumTo(n){
-//     let sum = 0;
-//  for(let i = 1;i <= n;i+=1){
-//    sum = sum + i;
+// console.log(count- sum);
+
+
+// let array = [2,3,4,1,10,9,7,5,6];
+// let sum = 0;
+// let count = 0;
+
+
+//  for(let i = 0; i < array.length;i++){
+//      count = i+count
+//         sum += array[i]
+//         if(i >= array.length-2){
+//         count += i+2;
+//        }
+    
 //  }
-//  return sum;
-// }
-// console.log(sumTo(100));
 
-// function factorial(n){
-//     let sum = 0;
-//     if(n == 1){
-//         return 1 ;
-//     }else{
-//        sum = n * factorial(n-1); 
+//  console.log(count- sum);
+
+
+// First exercises
+// let lines = 7;
+// let str = " ";
+// let star = "*";
+// for(let i = 0; i < lines; i++){
+//     str += star;
+//     console.log(str);
+// }
+// Second exercises
+// for(let i = 1;i <= 100;i++){
+//     if(i % 3 === 0){
+//         console.log("Fizz")
 //     }
-//     return sum;
+//     else if(i % 5 === 0){
+//         console.log("Buzz")
+//     }
+//     else{
+//         console.log(i);
+//     }
 // }
-// console.log(factorial(5));
 
-// function fib(n) {
-//     return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+// for(let i = 1;i<=100;i++){
+//     if(i % 3 == 0 && i % 5 == 0){
+//         console.log("FizzBuzz");
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// function min(a,b){
+//    if(a > b) {
+//        console.log(a);
+//    } else if(b>a){
+//        console.log(b);
+//    }    
+//    else{
+//        console.log("ERKU TVERY HAVASAREN")
+//    }
+// }
+// console.log(min(10,10));
+
+// let arr = [10,20,30,49,50];
+// let sum = 0;
+// for(let i of arr.length){
+//       if(i > arr[0] + 1){
+//           i = sum;
+//           console.log("amenamec tivy" + sum);
+//       }
+//       else if ([i] < arr[0]+1){
+//           i++;
+//       }
+//       else{
+//           console.log("this is equal numbers");
+//       }
+// }
+
+
+// let list = [34, 1, 529, -78, 89.7, 1243, 2000, 300000]; 
+// function maxElement (list){
+//     let max;
+//     let min = list[0];
+//     for(let i = 0; i < list.length; i++){
+//         if((list[i]) < min){
+//             min = list[i]; 
+//         } else {
+//             max = list[i];
+//         }
+//     }
+//     return max;
+// }
+
+// console.log(maxElement(list))
+
+
+                             /// Prototypes 
+// let mercedesA ={
+//     color:"black",
+//     inside:"true",
+//     outside:"false",
+//      driveCar(){
+//         console.log("Good driver");
+//     }
+// }                             
+
+// let mercedesS = {
+//     color:"black",
+//     __proto__ : mercedesA,
+// }
+
+// // mercedesS.__proto__ = mercedesA;
+// console.log(mercedesS.driveCar());
+
+// __proto equal === only Null or Object
+
+
+// let houseFirst ={
+//     place:"100",
+//     window:"two",
+//     openDoor(){
+//         console.log("open the door");
+//     }
+// }
+
+// let houseSecond = {
+//     place:"140",
+//     window:"four",
+// }
+
+
+// houseSecond.openDoor = function(){
+//     console.log("open the window");
+// }
+
+// houseSecond.__proto__ = houseFirst;
+// console.log(houseSecond.openDoor());
+
+
+
+// let animal = {  //// ?????
+//   walk() {
+//     if (!this.isSleeping) {
+//       console.log(`I walk`);
+//     }
+//   },
+//   sleep() {
+//     this.isSleeping = true;
 //   }
-// console.log(fib(5));
+// };
 
-// function fib(n) {
-//     let a = 1;
-//     let b = 1;
-//     for (let i = 3; i <= n; i++) {
-//       let c = a + b;// 2+3
-//       a = b;//2
-//       b = c; // 3
+// let rabbit = {
+//   name: "White Rabbit",
+//   __proto__: animal
+// };
+
+// // модифицирует rabbit.isSleeping
+// rabbit.walk();
+
+// console.log(rabbit.isSleeping); // true
+
+
+// let houseFirst ={
+//         place:"100",
+//         window:"two",
+//         openDoor(){
+//             console.log("open the door");
+//         }
 //     }
-//     return b;
-//   }
-  
-//  console.log( fib(5));
-
-// function sumAll(...args) { 
-//     let sum = 0;
-//     for (let arg of args) {
-//         sum = sum + arg;
+   
+//     let houseSecond = {
+//         place:"140",
+//         window:"four",
+//         __proto__:houseFirst,
 //     }
-  
-//     return sum;
-//   }
-  
-//   console.log(sumAll(1,2,3,4,6));
 
-// function randomName(){
-//   console.log(arguments[0])
+// console.log(Object.keys(houseSecond));
+// for( let user in houseSecond)console.log(user);
+
+
+                           // Getter (get) and Setter ( Set)
+// let user = {
+//     name:"Sergey",
+//     age:20,
+                        
+// get fullInfo(){
+//      return ` name ${this.name} age ${this.age}`;
+//  }
 // }
- 
-// console.log(randomName("Sergey","Karen",))
-
-// let arr1 = [1,2,3,6,8,0,12,56];
-// let arr2 = [4,12,33,45];
-
-// console.log(Math.max(...arr1,...arr2,));
-
-// let arr1 = [1,2,3,6,8,0,12,56];
-//  let arr2 = [4,12,33,45];
-
-//  let array = [...arr1,...arr2]
-// console.log(array);
-
-// let str = "Sergey";
-
-// for(let x of str){
-//      console.log(x);
-// }
-
-//let str = "Sergey";
-//console.log([...str]);
-
-//let str = "Hello";
-//console.log( Array.from(str) );
-//Если ... располагается в конце списка аргументов функции, то это «остаточные параметры». Он собирает остальные неуказанные аргументы и делает из них массив.
-//Если ... встретился в вызове функции или где-либо ещё, то это «оператор расширения». Он извлекает элементы из массива.
-
-// (function  (){
-//   console.log("hi");
-// })();
-
-// !function(){
-//     console.log("hi");
-// }();
-
-// var str = "Sergey";
-// console.log(window.str);
+// console.log(user.fullInfo);
 
 
-// let namesJson = {
-//     name: "Mike",
-//     age: 30,
-//     anyObject:{
-//         name:"Carol",
-//         age: 23,
-//     }
-// }
-// console.log(JSON.stringify(namesJson,undefined,2));
-
-// function sum(x){
-//    return function(y){
-//      return x + y ;
-//  }    
-// }
-
-// let count = sum(20);
-// console.log(count(10));
-
-// let person1 = ["Sergey","Karen","Armine"];
-// let person2 = ["Julia","Sergey"];
-
-// function userName(onePerson){
-//     return function(secondPerson){
-//      console.log(onePerson +" "+ secondPerson);
-//     }
-// }
-// let sum = userName(person1);
-// sum(person2);
-// console.log("111"[3]);
-// console.log("111"[2]);
-
-// let a = new Array(1,2);
-// let b = new Array(3);
-// console.log(a[0] + b [0]);
